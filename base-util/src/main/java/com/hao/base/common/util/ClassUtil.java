77f46807cdc794bbcd3103d860744f3c477822d2
hao <Lyinghao@126.com>
2023-12-14 11:36:18 +0800

    更新工具类

--- base-util/src/main/java/com/hao/base/common/util/ClassUtil.java
+++ base-util/src/main/java/com/hao/base/common/util/ClassUtil.java
@@ -1,6 +1,7 @@
 package com.hao.base.common.util;
 
 import java.lang.reflect.Field;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
---
+++ base-util/src/main/java/com/hao/base/common/util/CrudUtil.java
@@ -0,0 +1,48 @@
+package com.hao.base.common.util;
+
+import cn.hutool.core.util.ClassUtil;
+
+import java.lang.reflect.Method;
+import java.time.LocalDateTime;
+
+public class CrudUtil {
+
+    @SuppressWarnings("unchecked")
+    public static <T> T getId(Object entity) {
+        assert entity != null;
+        try {
+            Class<?> clazz = entity.getClass();
+            Method getId = ClassUtil.getPublicMethod(clazz, "getId");
+            return (T)getId.invoke(entity);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static <T> void setTime(T entity, LocalDateTime time) {
+
+        if (entity == null) {
+            return;
+        }
+
+        try {
+            Class<?> clazz = entity.getClass();
+
+            Object id = getId(entity);
+
+            if (id == null) {
+                // 是 insert
+                Method setCreateTime = ClassUtil.getPublicMethod(clazz, "setCreateTime", LocalDateTime.class);
+                setCreateTime.invoke(entity, time);
+            }
+
+            // 不管是 insert 还是 update 一定都会更新 updateTime
+            Method setUpdateTime = ClassUtil.getPublicMethod(clazz, "setUpdateTime", LocalDateTime.class);
+            setUpdateTime.invoke(entity, time);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
